694a9f179734f998fa2d9769bfffb7f7d0d53eab
Sajal Nema <sajal.nema@nucleusteq.com>
2024-11-14 16:15:35 +0530

    Add Code for Testing

---
+++ project/src/main/java/com/gradle/project/AppConfig.java
@@ -0,0 +1,35 @@
+package com.gradle.project;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Component
+@Configuration
+public class AppConfig {
+    @Value("${spring.application.name}")
+    private String appName;
+    
+    @Value("${liquibase.database}")
+    private String liquibasedb;
+    
+    @Value("${spring.datasource.url}")
+    private String db_url;
+    
+    @Value("${spring.datasource.username}")
+    private String db_username;
+    
+    @Value("${spring.datasource.password}")
+    private String db_password;
+     
+    @Bean
+    public AppConfig printProperties() {
+        System.out.println("Application Name: " + appName);
+        System.out.println("Database Value: " + liquibasedb);
+        System.out.println("Database URL: " + db_url);
+        System.out.println("Database Username: " + db_username);
+        System.out.println("Database Password: " + db_password);
+
+        return new AppConfig();
+    }
+}
---
+++ project/src/main/java/com/gradle/project/ProjectApplication.java
@@ -0,0 +1,14 @@
+package com.gradle.project;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
